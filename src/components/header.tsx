a95da4e851773794b648fd9f74bf787c57857008
muhammadramzan2803 <noureen.rafiq2@gmail.com>
2024-11-10 21:57:36 +0500

    first commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,28 +1,28 @@
 import Link from "next/link";
 import "../style/header.css";
 
-
-
 function Header(){
     return(
         <div className="header">
-            {/* left */}
-            <div>
-                <h1 className="logo">NR</h1>
-            </div>
-            
-            {/* right */}
-
-            <div className="header-right-div">
+             {/* left */}
+             <div>
+                <h1 className="logo">MA.</h1>
+             </div>
+             {/* right */}
+             <div className="header-right-div">
                 <ul className="header-links">
-                   <li><Link className="nav-links" href={"/"}>Home</Link></li>
-                   <li><Link className="nav-links" href={"/about"}>About</Link></li>
-                   <li><Link className="nav-links" href={"/contact"}>Contact</Link></li>
+                    <li>
+                        <Link className="nav-links" href={"/"}>Home</Link>
+                    </li>
+                    <li>
+                        <Link  className="nav-links" href={"/about"}>About</Link>
+                    </li>
+                    <li>
+                        <Link  className="nav-links" href={"/contact"}>Contact</Link>
+                    </li>
                 </ul>
-            </div>
-          
+             </div>
         </div>
     )
 }
-
-export default Header;
+export default Header
